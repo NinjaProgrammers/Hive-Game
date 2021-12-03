8f47b0e3ab74b2473708625b4821c900c7d37281
ClaudiaOM <claudiaomtnez@gmail.com>
2021-12-03 12:52:42 -0500

    fixed the evaluate_neighbors

--- computer.pl
+++ computer.pl
@@ -3,7 +3,7 @@
         alphabeta/5]).
 :- use_module([pieces,validate_moves,position,utils,adjacency]).
 
-moves(B,S) :- 
+moves(B,S) :-
         position(B,-1), !,
         color(B,C),
         insert_positions(C,S).
@@ -14,51 +14,51 @@ moves(B,[]) :-
         update_position(B,P),
         length(CC,L),
         L > 1, !.
-moves(B,S) :- 
+moves(B,S) :-
         type(B,bee),
         position(B,P),
         update_position(B,-1),
         moves_bee(P,T),
         update_position(B,P),
-        findall(X,(member(X,T),remain_connected(B,X)),S). 
-moves(B,S) :- 
+        findall(X,(member(X,T),remain_connected(B,X)),S).
+moves(B,S) :-
         type(B,beetle),
         position(B,P),
         update_position(B,-1),
         moves_beetle(P,T),
         update_position(B,P),
-        findall(X,(member(X,T),remain_connected(B,X)),S). 
-moves(B,S) :- 
+        findall(X,(member(X,T),remain_connected(B,X)),S).
+moves(B,S) :-
         type(B,grasshoper),
         position(B,P),
         update_position(B,-1),
         moves_grasshoper(P,T),
         update_position(B,P),
-        findall(X,(member(X,T),remain_connected(B,X)),S). 
-moves(B,S) :- 
+        findall(X,(member(X,T),remain_connected(B,X)),S).
+moves(B,S) :-
         type(B,spider),
         position(B,P),
         update_position(B,-1),
         moves_spider(P,T),
         update_position(B,P),
-        findall(X,(member(X,T),remain_connected(B,X)),S). 
-moves(B,S) :- 
+        findall(X,(member(X,T),remain_connected(B,X)),S).
+moves(B,S) :-
         type(B,ant),
         position(B,P),
         update_position(B,-1),
         moves_ant(P,T),
         update_position(B,P),
-        findall(X,(member(X,T),remain_connected(B,X)),S). 
-  
-available_moves(white,S) :- 
-        counter(6), 
+        findall(X,(member(X,T),remain_connected(B,X)),S).
+
+available_moves(white,S) :-
+        counter(6),
         position(white_bee,-1), !,
         insert_positions(white,L),
         reformat(white_bee,L,S).
-available_moves(white,S) :- !, 
+available_moves(white,S) :- !,
         available_moves_white(S).
-available_moves(black,S) :- 
-        counter(7), 
+available_moves(black,S) :-
+        counter(7),
         position(black_bee,-1), !,
         insert_positions(black,L),
         reformat(black_bee,L,S).
@@ -80,12 +80,12 @@ available_moves_white(S) :-
         white_on_board(0), !,
         findall(X,(piece(X),color(X,white)),Y),
         first_move_white(Y, S).
-available_moves_white(S) :- 
+available_moves_white(S) :-
         findall(X,(piece(X),color(X,white),position(X,-1)),Z),
         insert_positions(white,T),
         multi_reformat(Z,T,Q),
         findall(X,(piece(X),color(X,white),position(X,P),P =\= -1),Y),
-        available_moves_white_visit(Y,W), 
+        available_moves_white_visit(Y,W),
         append(Q,W,S), !.
 available_moves_white_visit([],[]).
 available_moves_white_visit(_,[]) :-
@@ -95,7 +95,7 @@ available_moves_white_visit([X|S],T) :-
         P =\= -1,
         not(upper_bug(P,X)), !,
         available_moves_white_visit(S,T).
-available_moves_white_visit([X|P],S) :- 
+available_moves_white_visit([X|P],S) :-
         moves(X,T),
         reformat(X,T,W),
         available_moves_white_visit(P,Q),
@@ -108,12 +108,12 @@ available_moves_black(S) :-
         black_on_board(0), !,
         findall(X,(piece(X),color(X,black)),Y),
         first_move_black(Y, S).
-available_moves_black(S) :- 
+available_moves_black(S) :-
         findall(X,(piece(X),color(X,black),position(X,-1)),Z),
         insert_positions(black,T),
         multi_reformat(Z,T,Q),
         findall(X,(piece(X),color(X,black),position(X,P),P =\= -1),Y),
-        available_moves_black_visit(Y,W), 
+        available_moves_black_visit(Y,W),
         append(Q,W,S), !.
 available_moves_black_visit(_,[]) :-
         position(black_bee,-1), !.
@@ -123,7 +123,7 @@ available_moves_black_visit([X|S],T) :-
         P =\= -1,
         not(upper_bug(P,X)), !,
         available_moves_black_visit(S,T).
-available_moves_black_visit([X|P],S) :- 
+available_moves_black_visit([X|P],S) :-
         moves(X,T),
         reformat(X,T,W),
         available_moves_black_visit(P,Q),
@@ -142,12 +142,12 @@ boundedbest(Depth,[(B:P)|L],Alpha,Beta,BetterMove,Value) :-
         undo_move,
         goodenough(Depth,L,Alpha,Beta,(B:P),V,BetterMove,Value).
 goodenough(_,[],_,_,Pos,Val,Pos,Val) :- !.
-goodenough(_,_,Alpha,Beta,Pos,Val,Pos,Val) :- 
+goodenough(_,_,Alpha,Beta,Pos,Val,Pos,Val) :-
         (turn(white), Val > Beta), !;
         (turn(black), Val < Alpha), !.
 goodenough(Depth,L,Alpha,Beta,Pos,Val,BetterMove,Value) :-
         updatealphabeta(Alpha,Beta,NAlpha,NBeta),
-        boundedbest(Depth,L,NAlpha,NBeta,P1,V1), 
+        boundedbest(Depth,L,NAlpha,NBeta,P1,V1),
         betterof(Pos,Val,P1,V1,BetterMove,Value).
 
 betterof(Pos,Val,_,Val1,Pos,Val) :-
@@ -161,7 +161,7 @@ updatealphabeta(Alpha, Beta, NAlpha, NBeta) :-
         NBeta is max(0, Bt).
 
 
-        
+
 evaluate_pieces_out(R) :-
         findall(X,(piece(X),color(X,white),position(X,-1)),W),
         findall(X,(piece(X),color(X,black),position(X,-1)),B),
@@ -178,7 +178,7 @@ evaluate_pieces_blocked(R) :-
         length(B,Q),
         R is 6 * (Q - P).
 
-evaluate_neighbor_bee(R) :-  
+evaluate_neighbor_bee(R) :-
         evaluate_neighbor_white_bee(P),
         evaluate_neighbor_black_bee(Q),
         %white_on_board(W),
@@ -195,28 +195,24 @@ evaluate_neighbor_black_bee(R) :-
         (findall(X,(adjacent(P,X),occupied(X)),Y),
         length(Y,T)), R is T * T).
 
-evaluate(0) :-
-        draw, !.
-evaluate(-1000) :-
-        white_lose, !.
-evaluate(1000) :-
-        black_lose, !.
-evaluate(R):- 
+greater(W,B,R) :-
+        W > B -> 
+        R is W * -2;
+        R is B * 2.
+evaluate(R):-
+        X is 0,
+        Y is 0,
+        ((white_lose -> X is -20);
+        (black_lose -> Y is 20)),
+        R is X + Y.
+evaluate(R):-
         evaluate_neighbor_white_bee(W),
         evaluate_neighbor_black_bee(B),
-        W > B, W >= 4, !,
-        R is -W * 100.
-evaluate(R):- 
-        evaluate_neighbor_white_bee(W),
-        evaluate_neighbor_black_bee(B),
-        W < B, B >= 4, !,
-        R is B * 100.
-evaluate(R) :- 
+        W =\= B, max(W,B) >= 4, !,
+        greater(W,B,R).
+evaluate(R) :-
         evaluate_pieces_out(Po),
         evaluate_pieces_blocked(Pb),
         R is Po + Pb.
-        
-
-
 
 
